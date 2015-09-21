7958e2bf742f460756b37a31fe0297dd56891408
Shawn <shawnhmh@gmail.com>
2015-09-21 01:32:51 -0400

    Merging migrations

---
+++ articles/migrations/0020_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('articles', '0019_auto_20150919_1726'),
+        ('articles', '0019_auto_20150919_1728'),
+    ]
+
+    operations = [
+    ]
