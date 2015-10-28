464fb2ef7173eeba6e3e050c39262811172c1c5c
Shawn <shawnhmh@gmail.com>
2015-10-28 14:11:45 -0400

    Added 404

--- sbomb/urls.py
+++ sbomb/urls.py
@@ -17,17 +17,27 @@ from django.conf import settings
 from django.conf.urls import include, url, static
 from django.contrib import admin
 from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
 
 import main.views
 
 urlpatterns = [
+   # url(r'^contribute/account/login/$', auth_views.login, {'template_name': 'account/login.html'}, name='login'),
+   # url(r'^contribute/account/logout/$', auth_views.logout, {'template_name': 'account/logout.html'}, name='logout'),
+    url(r'^contribute/account/password_change/$', auth_views.password_change, {'template_name': 'account/password_change_form.html'}, name='password_change'),
+    url(r'^contribute/account/password_change/done/$', auth_views.password_change_done, {'template_name': 'account/password_change_done.html'}, name='password_change_done'),
+    url(r'^contribute/forgot-password/$', auth_views.password_reset, {'template_name': 'account/password_reset_form.html'}, name='password_reset'),
+    url(r'^contribute/account/password_reset/done/$', auth_views.password_reset_done, {'template_name': 'account/password_reset_done.html'}, name='password_reset_done'),
+    url(r'^contribute/account/reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.password_reset_confirm, {'template_name': 'account/password_reset_confirm.html'}, name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.password_reset_complete, {'template_name': 'account/password_reset_complete.html'}, name='password_reset_complete'),
     url(r'^$', main.views.index, name='index'),
-    url(r'^about_us/$', TemplateView.as_view(template_name='main/about_us.html'), name='about_us'),
+    url(r'^about-us/$', TemplateView.as_view(template_name='main/about_us.html'), name='about_us'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^ckeditor/', include('ckeditor.urls')),
     url(r'^articles/', include('articles.urls', namespace='articles')),
     url(r'^profiles/', include('profiles.urls', namespace='profiles')),
     url(r'^bands/', include('bands.urls', namespace='bands')),
     url(r'^feedback/', include('feedback.urls', namespace='feedback')),
-    url(r'^contribute/', include('contribute.urls', namespace='contribute')),
+    url(r'^contribute/', include('contribute.urls', app_name='contribute', namespace='contribute')),
 ] + static.static( settings.MEDIA_URL, document_root=settings.MEDIA_ROOT )
