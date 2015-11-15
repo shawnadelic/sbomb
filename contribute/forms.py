d05654c633df8065bd982127690157df4bd37215
Shawn <shawnhmh@gmail.com>
2015-11-15 16:48:07 -0500

    Updated contribute templates

--- contribute/forms.py
+++ contribute/forms.py
@@ -3,6 +3,7 @@ from articles.models import Article
 from ckeditor.widgets import CKEditorWidget
 
 class ArticleForm(forms.ModelForm):
+    #body = forms.CharField(widget=CKEditorWidget(config_name='default'))
     body = forms.CharField(widget=CKEditorWidget(config_name='contributor_editor'))
     class Meta:
         model = Article
