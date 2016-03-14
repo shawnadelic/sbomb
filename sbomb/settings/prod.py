4514d6cd6dc3242673b115505e846ef0801bdf7e
Shawn Her Many Horses <shawnhmh@gmail.com>
2016-03-13 18:29:56 -0700

    Add local and production settings files, requiring ``SBOMB_SECRET_KEY`` to be set in OS environmental variables.

---
+++ sbomb/settings/prod.py
@@ -0,0 +1,15 @@
+from base import *
+
+# Database
+# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django',
+	'USER': 'django',
+	'PASSWORD': 'WNmoQiJhmo',
+	'HOST': 'localhost',
+	'PORT': '',
+    }
+}
